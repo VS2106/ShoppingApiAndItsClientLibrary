8605c35472ca658b517fb438217d2ee95508d5e5
SongWenhao <swh2106@hotmail.com>
2018-02-11 17:40:01 +0100

    setup dependency injection tool Ninject, set its scoped to per HTTP requests, bing all interfaces and classes by naming convention (eg. IPeople with People ).

---
+++ ShoppingAPI/App_Start/NinjectWebCommon.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+using Ninject;
+using Ninject.Extensions.Conventions;
+using Ninject.Web.Common;
+using Ninject.Web.Common.WebHost;
+using ShoppingAPI.App_Start;
+using System;
+using System.Web;
+
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(NinjectWebCommon), "Start")]
+[assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(NinjectWebCommon), "Stop")]
+namespace ShoppingAPI.App_Start
+{
+    /// <summary>
+    /// Bootstrapper for the application.
+    /// </summary>
+    public static class NinjectWebCommon
+    {
+        private static readonly Bootstrapper bootstrapper = new Bootstrapper();
+
+        /// <summary>
+        /// Starts the application
+        /// </summary>
+        public static void Start()
+        {
+            DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
+            DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
+            bootstrapper.Initialize(CreateKernel);
+        }
+
+        /// <summary>
+        /// Stops the application.
+        /// </summary>
+        public static void Stop()
+        {
+            bootstrapper.ShutDown();
+        }
+
+        /// <summary>
+        /// Creates the kernel that will manage your application.
+        /// </summary>
+        /// <returns>The created kernel.</returns>
+        private static IKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+            kernel.Bind<Func<IKernel>>().ToMethod(ctx => () => new Bootstrapper().Kernel);
+            kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
+
+            RegisterServices(kernel);
+
+            kernel.Bind(x =>
+            {
+                x.FromThisAssembly()
+                    .SelectAllClasses()
+                    .BindDefaultInterface();
+            });
+
+            return kernel;
+        }
+
+        /// <summary>
+        /// Load your modules or register your services here!
+        /// </summary>
+        /// <param name="kernel">The kernel.</param>
+        private static void RegisterServices(IKernel kernel)
+        {
+            //kernel.Load(Assembly.GetExecutingAssembly());
+        }
+    }
+}
