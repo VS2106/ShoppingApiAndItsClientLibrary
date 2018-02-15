fbc32c52b30c240227d9e2215f3b901c46e2d3f4
SongWenhao <swh2106@hotmail.com>
2018-02-15 15:33:07 +0100

    add product controller

---
+++ ShoppingAPI/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using AutoMapper;
+using ShoppingAPI.Core;
+using ShoppingAPI.Core.Dtos;
+using ShoppingAPI.Core.Models;
+
+namespace ShoppingAPI.Controllers
+{
+    [Authorize]
+    public class ProductsController : ApiController
+    {
+        private IUnitOfWork _unitOfWork;
+
+        public ProductsController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IHttpActionResult Get()
+        {
+            return Ok(Mapper.Map<List<Product>, List<ProductDto>>(_unitOfWork.Products.GetAll().ToList()));
+        }
+    }
+}
