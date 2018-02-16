082b69863649017f6d14db7265e6203a0e0a3e89
SongWenhao <swh2106@hotmail.com>
2018-02-16 15:26:18 +0100

    Add shopping basket controller, to get or clean out shopping basket for current user.

---
+++ ShoppingAPI/Controllers/ShoppingBasketController.cs
@@ -0,0 +1,48 @@
+﻿using System.Web.Http;
+using AutoMapper;
+using Microsoft.AspNet.Identity;
+using ShoppingAPI.Core;
+using ShoppingAPI.Core.Dtos;
+using ShoppingAPI.Core.Models;
+
+namespace ShoppingAPI.Controllers
+{
+    [Authorize]
+    public class ShoppingBasketController : ApiController
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ShoppingBasketController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IHttpActionResult Get()
+        {
+            var shoppingBasket = _unitOfWork.ShoppingBaskets.Find(User.Identity.GetUserId());
+
+            if (shoppingBasket == null)
+                //shoppingBasket can never be null.
+                //Every user must have one shopping basket
+                //That's why here use InternalServerError instead of NotFound
+                return InternalServerError();
+
+            return Ok(Mapper.Map<ShoppingBasket, ShoppingBasketDto>(shoppingBasket));
+        }
+
+        [Route("shoppingbasket/clearout")]
+        [HttpPut]
+        public IHttpActionResult ClearOut()
+        {
+            var shoppingBasket = _unitOfWork.ShoppingBaskets.Find(User.Identity.GetUserId());
+
+            if (shoppingBasket == null)
+                return InternalServerError();
+
+            shoppingBasket.OrderItems.Clear();
+            _unitOfWork.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
