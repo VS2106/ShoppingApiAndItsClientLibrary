d52b20b538fea8fbeda905b536bcb7f38d83dfa0
SongWenhao <swh2106@hotmail.com>
2018-02-24 17:41:43 +0100

    add unit test for Dto model

---
+++ ShoppingAPI.Tests/Dtos/OrderItemPutDtoTest.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShoppingAPI.Core.Dtos;
+
+namespace ShoppingAPI.Tests.Dtos
+{
+    [TestClass]
+    public class OrderItemPutDtoTest
+    {
+        [TestMethod]
+        public void SetQuantityOfOrderItemPutDtoSmallerThan1_ModelStateShouldBeNotValid()
+        {
+            var orderItemPutDto = new OrderItemPutDto()
+            {
+                Quantity = 0
+            };
+
+            var context = new ValidationContext(orderItemPutDto, null, null);
+            var results = new List<ValidationResult>();
+            var isModelStateValid = Validator.TryValidateObject(orderItemPutDto, context, results, true);
+
+            isModelStateValid.Should().Be(false);
+        }
+
+        [TestMethod]
+        public void SetValidValueForQuantityOfOrderItemPutDto_ModelStateShouldBeValid()
+        {
+            var orderItemPutDto = new OrderItemPutDto()
+            {
+                Quantity = 5
+            };
+
+            var context = new ValidationContext(orderItemPutDto, null, null);
+            var results = new List<ValidationResult>();
+            var isModelStateValid = Validator.TryValidateObject(orderItemPutDto, context, results, true);
+
+            isModelStateValid.Should().Be(true);
+        }
+    }
+}
